14da55cf52352884c06a9ad905cb9af053981db8
robins <robinskumar73@gmail.com>
2015-02-01 10:07:24 +0530

    First commit

---
+++ scripts/my-script.js
@@ -0,0 +1,29 @@
+
+$(function(){
+    var nav = $('#nav-header'),
+        pos = nav.offset();
+
+        $(window).scroll(function(){
+            if($(this).scrollTop() > pos.top+nav.height() && nav.hasClass('title')){
+                nav.fadeOut('fast', function(){
+                    $(this).removeClass('title').addClass('fixed').fadeIn();
+					//$(this).addClass('block_top_nav');
+                });
+            } else if($(this).scrollTop() <= pos.top && nav.hasClass('fixed')){
+                nav.fadeOut('fast', function(){
+					//$(this).removeClass('block_top_nav');
+                    $(this).removeClass('fixed').addClass('title').fadeIn();
+                });
+				
+            }
+        });
+
+});
+
+$(function() {
+    $( "#slider" ).slider({
+			value: 100,
+            animate:"fast",
+            orientation: "horizontal"
+	});
+  });
