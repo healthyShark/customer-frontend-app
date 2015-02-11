857e968e8b63a4982c3ab19e83c4b1c8248e26d7
ravi gupta <ravigupta9363@gmail.com>
2015-02-11 13:35:37 +0530

    "Note Style is added"

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -57,3 +57,27 @@ $("#tabstrip").switchButton({
 	height:15,
 	button_width: 25,
 });
+
+
+function divClicked() {
+    var divHtml = $(this).html();
+    var editableText = $("<textarea />");
+    editableText.val(divHtml);
+    $(this).replaceWith(editableText);
+    editableText.focus();
+    // setup the blur event for this new textarea
+    editableText.blur(editableTextBlurred);
+}
+
+function editableTextBlurred() {
+    var html = $(this).val();
+    var viewableText = $("<div>");
+    viewableText.html(html);
+    $(this).replaceWith(viewableText);
+    // setup the click event for this new div
+    viewableText.click(divClicked);
+}
+
+$(document).ready(function() {
+    $(".prescription-notes").click(divClicked);
+});
